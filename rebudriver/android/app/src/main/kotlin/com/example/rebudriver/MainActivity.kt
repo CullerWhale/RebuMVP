e5804349c35e5ea4a917ade5646bc6da2a1c31f6
cullen harris <jcullenharris@gmail.com>
2025-01-25 18:34:00 -0600

    init init

---
+++ rebudriver/android/app/src/main/kotlin/com/example/rebudriver/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rebudriver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ reburider/android/app/src/main/kotlin/com/example/reburider/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reburider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
